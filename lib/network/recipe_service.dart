2402b22dffd9b87acdfa28d44d6012ee070aa7b3
Arthur Puha <pugaartur@gmail.com>
2021-10-13 12:15:29 +0300

    search and api

---
+++ lib/network/recipe_service.dart
@@ -0,0 +1,32 @@
+import 'package:http/http.dart';
+
+const String apiKey = '3c9a8ea32f378ba4a8dda11d25c1f79c';
+const String apiId = '2af57395';
+const String apiUrl = 'https://api.edamam.com/search';
+
+class RecipeService {
+  // 1
+  Future getData(String url) async {
+    // 2
+    print('Calling url: $url');
+    // 3
+    final response = await get(Uri.parse(url));
+    // 4
+    if (response.statusCode == 200) {
+      // 5
+      return response.body;
+    } else {
+      // 6
+      print(response.statusCode);
+    }
+  }
+// 1
+  Future<dynamic> getRecipes(String query, int from, int to) async {
+    // 2
+    final recipeData = await getData(
+        '$apiUrl?app_id=$apiId&app_key=$apiKey&q=$query&from=$from&to=$to');
+    // 3
+    return recipeData;
+  }
+
+}
